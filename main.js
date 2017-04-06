132b685b25bca71db48584ccec366334857e1ba6
Nicholas Carey <ncarey@Nicks-MacBook-Pro.local>
2017-04-06 15:00:51 -0400

    final version of code?

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+	var averageWater = {
+		"average": 560,
+	}
+
 	$("#submit").on("click", function (e) {
 		e.preventDefault();
 
@@ -14,22 +18,31 @@ $(document).ready(function () {
 		var number9 = parseFloat($("#car").val());
 		var number10 = parseFloat($("#lawn").val());
 		var number11 = parseFloat($("#cups").val());
+		var average = 700;
 
-		if (operation != "add") {
-			console.log("You are NOT performing addition");
-		} else {
-			console.log("Hey, we're adding");
-		}
-
-		if (operation == "add") {
-			var sum = addition(number1, number2, number3, number4, number5, number6, number7, number8, number9, number10, number11)
-		} 
+		var sum = addition(number1, number2, number3, number4, number5, number6, number7, number8, number9, number10, number11);
+	
 
 		$("#sum").val(sum);
+
+		$(".results").text(writeText(sum));
+
+
+		function writeText(sum) {
+			if (sum > average) {
+				return "The average American uses around 700 gallons of water a week. You're using more water than that! Check out the tips to cut down below."
+			} else if (sum < average) {
+				return "Good job! The average American uses around 700 gallons of water a week. You're using less than that. Check out some extra water-saving tips below."
+			} else if (sum == average) {
+				return "You're average! Most Americans use around 700 gallons of water a week. Check out the tips to cut down below."
+			};
+
+		};
 	});
 });
 
 function addition(shower, toilet, teeth, hands, dishes, dishwasher, cooking, clothes, car, lawn, cups) {
-	var sum = ((shower * 21) + (toilet * 3.5) + (teeth * .5) + (hands * 2) + (dishes * 4.4) + (dishwasher * 10) + (cooking * 2) + (clothes * 40) + (car * 32) + (lawn * 32) + (cups * .1)) * 4;
+	var sum = (shower * 21) + (toilet * 3.5) + (teeth * .5) + (hands * 2) + (dishes * 4.4) + (dishwasher * 10) + (cooking * 2) + (clothes * 40) + (car * 32) + (lawn * 32) + (cups * .1);
 	return sum;
 }
+
